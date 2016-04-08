ec6e5e984017925f7427792b940fa480eec611e4
Brian <Brian@10.148.86.22>
2016-04-08 15:09:05 -0500

    Added KeyListener Implementation

--- src/javaGame/JavaGame.java
+++ src/javaGame/JavaGame.java
@@ -1,20 +1,51 @@
 package javaGame;
 
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 
 public class JavaGame extends JFrame{
 	
+	int x, y;
+	
+	public class AL extends KeyAdapter{
+		public void keyPressed(KeyEvent e){
+			int keyCode = e.getKeyCode();
+			if(keyCode == e.VK_LEFT){
+				x--;
+			}
+			if(keyCode == e.VK_RIGHT){
+				x++;
+			}
+			if(keyCode == e.VK_UP){
+				y--;
+			}
+			if(keyCode == e.VK_DOWN){
+				y++;
+			}
+		}
+		public void keyReleased(KeyEvent e){
+			
+		}
+	}
+	
 	public JavaGame(){
+		addKeyListener(new AL());
 		setTitle("Java Game");
 		setSize(250, 250);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		x = 150; 
+		y = 150;
 	}
 	
 	public void paint(Graphics g){
-		g.drawString("Hello World", 75, 75);
+		g.fillOval(x, y, 15, 15);
+		
+		repaint();
 	}
 	
 	public static void main(String[] args){
