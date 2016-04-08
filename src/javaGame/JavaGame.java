3270a923fb8846c5df2cfab52ff3e8dbc96d9685
Brian <Brian@10.148.86.22>
2016-04-08 15:14:01 -0500

    Added Double Buffering

--- src/javaGame/JavaGame.java
+++ src/javaGame/JavaGame.java
@@ -1,6 +1,7 @@
 package javaGame;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
@@ -8,6 +9,8 @@ import javax.swing.JFrame;
 public class JavaGame extends JFrame{
 	
 	int x, y;
+	private Image dbImage;
+	private Graphics dbg;
 	
 	public class AL extends KeyAdapter{
 		public void keyPressed(KeyEvent e){
@@ -43,6 +46,13 @@ public class JavaGame extends JFrame{
 	}
 	
 	public void paint(Graphics g){
+		dbImage = createImage(getWidth(), getHeight());
+		dbg = dbImage.getGraphics();
+		paintComponent(dbg);
+		g.drawImage(dbImage, 0, 0, this);
+	}
+	
+	public void paintComponent(Graphics g){
 		g.fillOval(x, y, 15, 15);
 		
 		repaint();
