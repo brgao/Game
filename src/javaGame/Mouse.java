28c4c124f2305d26d1aac04b2d7e71dd09a4785c
Brian <briangao343@gmail.com>
2016-04-23 22:30:48 -0500

    1

--- src/javaGame/Mouse.java
+++ src/javaGame/Mouse.java
@@ -7,7 +7,7 @@ import java.awt.event.MouseMotionListener;
 import javax.swing.JFrame;
 
 
-//12344
+//123456
 public class Mouse extends JFrame implements MouseMotionListener{
 	private Image dbImage;
 	private Graphics dbg;
